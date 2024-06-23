dedcd8d4cf6fc85a7c3586870231765ba6381cfe
yashasvi <yashasvi.yadav@ebizondigital.com>
2024-06-23 21:47:12 +0530

    deployment

--- src/components/dashboard/activity/index.js
+++ src/components/dashboard/activity/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
-
-import './activity.scss'
+import Image from 'next/image';
+import './activity.scss';
+import activity1 from '../../../images/img-1.jpeg';
+import activity2 from '../../../images/img-2.jpeg';
+import activity3 from '../../../images/img-3.jpeg';
+import activity4 from '../../../images/img-4.jpeg';
 
 const Activity = () => {
     return(
@@ -10,41 +14,45 @@ const Activity = () => {
             <div className='activityInner'>
                 <div className='activityBox'>
                     <div class="video-container">
-                        <iframe width="280" height="157" src="https://www.youtube.com/embed/Gwed919jYhw?si=GCswAGNAN-04A-L8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                        <Image src={activity1} alt="Self Assess<" />
+                        {/* <iframe width="280" height="157" src="https://www.youtube.com/embed/Gwed919jYhw?si=GCswAGNAN-04A-L8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe> */}
                     </div>
                     <div className='title'>Healing Power</div>
                     <div className='timeAudio'>
-                        <span>2:30 m.</span>
+                        <span>10:30 m.</span>
                         <span>Audio</span>
                     </div>
                 </div>  
                 <div className='activityBox'>
                     <div class="video-container">
-                        <iframe width="280" height="157" src="https://www.youtube.com/embed/WPPPFqsECz0?si=Sjnep6imrTpb3mQe" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                        <Image src={activity2} alt="Self Assess<" />
+                        {/* <iframe width="280" height="157" src="https://www.youtube.com/embed/WPPPFqsECz0?si=Sjnep6imrTpb3mQe" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe> */}
                     </div>
                     <div className='title'>An Antidote</div>
                     <div className='timeAudio'>
-                        <span>2:30 m.</span>
+                        <span>6:55 m.</span>
                         <span>Audio</span>
                     </div>
                 </div>  
                 <div className='activityBox'>
                     <div class="video-container">
-                        <iframe width="280" height="157" src="https://www.youtube.com/embed/75d_29QWELk?si=0pTPkWHrbPh8xgOg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                        <Image src={activity3} alt="Self Assess<" />
+                        {/* <iframe width="280" height="157" src="https://www.youtube.com/embed/75d_29QWELk?si=0pTPkWHrbPh8xgOg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe> */}
                     </div>
                     <div className='title'>Change Your Life</div>
                     <div className='timeAudio'>
-                        <span>2:30 m.</span>
+                        <span>8:10 m.</span>
                         <span>Audio</span>
                     </div>
                 </div>  
                 <div className='activityBox'>
                     <div class="video-container">
-                        <iframe width="280" height="157" src="https://www.youtube.com/embed/lvgM39UgHbA?si=CV27f8fmkvufqdL6" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                        <Image src={activity4} alt="Self Assess<" />
+                        {/* <iframe width="280" height="157" src="https://www.youtube.com/embed/lvgM39UgHbA?si=CV27f8fmkvufqdL6" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe> */}
                     </div>
                     <div className='title'>Getting Better</div>
                     <div className='timeAudio'>
-                        <span>2:30 m.</span>
+                        <span>5:15 m.</span>
                         <span>Audio</span>
                     </div>
                 </div>  
---
+++ src/constant/dashboardMiniGuide.js
@@ -0,0 +1,27 @@
+export const DashboardMiniGuideLinks = [
+    {
+       link: "https://www.youtube.com/watch?v=pRynMDAKrvI",
+       title: "How To Create Happiness In Your Life",
+       heading: "Create Happiness",
+       time: "9:59"
+    },
+    {
+        link: "https://www.youtube.com/watch?v=2oLdlthQZcg",
+        title: "How be happy and joyful",
+        heading: "Overcoming Self-Hatred",
+        time: "9:16"
+    },
+    {
+        link: "https://www.youtube.com/watch?v=QSCXyYuT2rE",
+        title: "Big things you ought to achieve",
+        heading: "The Body Keeps the Score",
+        time: "11:30"
+    },
+    {
+        link: "https://www.youtube.com/watch?v=U5MfuPpjefI",
+        title: "Top 3 habits to create happiness in your life",
+        heading: "How I Learned To Love Myself",
+        time: "5:31"
+    },
+
+]

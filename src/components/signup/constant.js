babe53875eabd1a98b6df6759a532f6c1de4a28e
yashasvi yadav <yashasvi.yadav@ebizondigital.com>
2024-12-02 20:00:57 +0530

    updated schools

--- src/components/signup/constant.js
+++ src/components/signup/constant.js
@@ -29,4 +29,64 @@ export const schools = [
         id: '66cb6cd3c4f8d9db008f91cb',
         name: 'G.D.Goenka Public School'
     },
+    {
+        id: '674dbff8c4195df29b44c4ef',
+        name: 'R.R. Inter College'
+    },
+    {
+        id: '674dbff8c4195df29b44c4f2',
+        name: 'G.I.C. Inter College'
+    },
+    {
+        id: '674dbff8c4195df29b44c4f5',
+        name: 'G.G.I.C. Inter College'
+    },
+    {
+        id: '674dbff8c4195df29b44c4f8',
+        name: 'Sanatan Dharma Inter College'
+    },
+    {
+        id: '674dbff8c4195df29b44c4fb',
+        name: 'A.S.V.V. Inter College'
+    },
+    {
+        id: '674dbff8c4195df29b44c4fe',
+        name: 'Veni Madhav Inter College'
+    },
+    {
+        id: '674dbff8c4195df29b44c501',
+        name: 'Arya Kanya Inter College'
+    },
+    {
+        id: '674dbff8c4195df29b44c504',
+        name: 'Ganga Devi Inter College'
+    },
+    {
+        id: '674dbff8c4195df29b44c507',
+        name: 'Saraswati Vidya Mandir High School'
+    },
+    {
+        id: '674dbff9c4195df29b44c50a',
+        name: 'Rafi Ahmad Inter College'
+    },
+    {
+        id: '674dbff9c4195df29b44c50d',
+        name: 'Cane Grover Public School'
+    },
+    {
+        id: '674dbff9c4195df29b44c510',
+        name: 'Composite School Sector 12'
+    },
+    {
+        id: '674dbff9c4195df29b44c513',
+        name: 'Junior High School Sector 5'
+    },
+    {
+        id: '674dbff9c4195df29b44c516',
+        name: 'Composite School Chalera'
+    },
+    {
+        id: '674dbff9c4195df29b44c519',
+        name: 'Saraswati Vidya Mandir School'
+    }
 ];
